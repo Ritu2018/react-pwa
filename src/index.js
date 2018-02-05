b9e5d059280df71b77d018257b943414ef3f9c6d
hadeeb <hadeebfarhan1@gmail.com>
2018-02-06 03:43:07 +0530

    Removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import HomePage from './components/HomePage/index';
 import registerServiceWorker from './registerServiceWorker';
 
