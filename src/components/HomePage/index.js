4585a002ac08354122f87416e96034259b422bd8
hadeeb <hadeebfarhan1@gmail.com>
2018-02-09 22:40:52 +0530

    Fixed HomePage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,112 +1,61 @@
 import React ,{ Component } from "react";
+import {Redirect} from "react-router-dom";
 
 import './style.css';
-import initdraw from './splat';
-import Logo from '../../assets/ritu.svg';
-import SideButton from '../../assets/asset1.svg';
+import './pc.css';
+import Logo from '../../assets/hadeeb.png';
+import RITLogo from '../../assets/ritlogo.png';
+import bg from '../../assets/back.jpg';
 
 class HomePage extends Component {
-
-    componentDidMount() {
-        initdraw();
+    constructor(){
+        super();
+        this.state = ({
+            showMenu:false,
+            redirect:false
+        });
+        this.nextPage = null;
     }
-
-    showSideMenu() {
-        document.getElementsByClassName("sidemenu_about")[0].style.left = 0;
-        document.getElementsByClassName("sidemenu_social")[0].style.top = 0;
-        document.getElementsByClassName("overlay")[0].style.opacity = 1;
-        document.getElementsByClassName("overlay")[0].style.zIndex = 5;
-
-        document.getElementsByClassName("sidemenu_icon")[0].style.opacity = 0;
+    navigateTo(page) {
+        this.nextPage = page;
+        this.setState({redirect:true});
     }
-    hideSideMenu(){
-        document.getElementsByClassName("sidemenu_about")[0].style.left = -100 + '%';
-        document.getElementsByClassName("sidemenu_social")[0].style.top = -100 + '%';
-        document.getElementsByClassName("overlay")[0].style.opacity = 0 ;
-        document.getElementsByClassName("overlay")[0].style.zIndex = 0;
-        document.getElementsByClassName("sidemenu_icon")[0].style.opacity = 1;
-    }
-
     render() {
-        const circlebtn = 
-            <div className="circle-menubar-button">
-                <span className="fa-stack fa-2x">
-                    <i className="fas fa-circle fa-stack-2x" style={{color:'red'}}/>
-                    <i id="btn-text" className="fa-inverse fas fa-bars fa-stack-1x" data-fa-transform="shrink-6"/>
-                </span>
+        if(this.state.redirect && this.nextPage!=null)
+            return <Redirect push to={this.nextPage}/>;
+        let menu_style = this.state.showMenu?"menu show":"menu";
+        let ham_style = this.state.showMenu?"fas fa-times fa-stack-1x fa-inverse":"fas fa-bars fa-stack-1x fa-inverse";
+        let menu_overlay_style = this.state.showMenu?"menu-overlay blurred":"menu-overlay";
+        const menu =
+            <div className={menu_style}>
+                <div className="menu-btn yellow" onClick={()=>this.navigateTo("events")}><span className="menu-text">Events</span></div>
+                <div className="menu-btn red"><span className="menu-text">Workshops</span></div>
+                <div className="menu-btn blue"><span className="menu-text">Highlights</span></div>
+                <div className="menu-btn green"><span className="menu-text">Contact</span></div>
             </div>;
-        const mainmenu = 
-            <div className="main-container">
-                <div className="element-container-pc">
-                    <div className="main-element-pc">
-                        <i  className="main-element-icon"></i>
-                        <label className="main-element-title-pc">Events</label>
-                    </div>
-                    <div className="main-element-pc" >
-                        <i  className="main-element-icon"></i>
-                        <label className="main-element-title-pc">Workshops</label>
-                    </div>
-                    <div className="main-element-pc">
-                        <i  className="main-element-icon"></i>
-                        <label className="main-element-title-pc">Highlights</label>
-                    </div>
-                    <div className="main-element-pc">
-                        <i  className="main-element-icon"></i>
-                        <label className="main-element-title-pc">Registration</label>
-                    </div>
+        const sidemenu = 
+            <div className="second_menu">
+                <div className="social_icons">
+                </div>
+                <div className="extra_icons">
                 </div>
             </div>;
-
-        const sideicon = 
-            <div className="sidemenu_icon" id="sidemenu_fab" onClick={this.showSideMenu}>
-                <img src={SideButton} alt="" className="side-menu"/>
-            </div>;
-        const sidesocial = 
-            <div className="sidemenu_social">
-                <span><i className="fab fa-facebook"></i></span>
-                <span><i className="fab fa-instagram"></i></span>
-                <span><i className="fab fa-instagram"></i></span>
-            </div>;
-
-        const sideabout = 
-            <div className="sidemenu_about"/>;
-
-        const content = 
-            <div className="content">
-                <div className="test">
-                    <div className="img">
-                        <img src={Logo} alt=""/>
-                    </div>
-                    <div className="date">
-                        <span>Feb 28</span>
-                        <span>Mar 1,2</span>
-                    </div>
-
-                </div>
-
-                <div className="desc">
-                    <div className="footer">
-
-                    </div>
-                    <div className="container">
-                        <span className="typed">
-                                Techno Cultural Fest of RIT
-                        </span>
-                    </div>
-                </div>
-            </div>;
-        const canvas = <canvas id="canvas"></canvas>;
-        const overlay = <div className="overlay" onClick={this.hideSideMenu}></div>;
         return(
-        <div>
-            {circlebtn}
-            {mainmenu}
-            {sideicon}
-            {sideabout}
-            {sidesocial}
-            {canvas}
-            {overlay}
-            {content}
+        <div className="container">
+            <img className="bg" src={bg} alt=""/>
+            <div className="bg-overlay"/>
+            <div className={menu_overlay_style}/>
+            <div className="logo-container">
+                <img className="logo" src={Logo} alt=""/>
+            </div>
+            <div className="ham-btn" onClick={()=>this.setState({showMenu:!this.state.showMenu})}>
+                <span class="fa-stack fa-2x">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class={ham_style}></i>
+                </span>
+            </div>
+            {menu}
+            <img className="rit-logo" src={RITLogo} alt=""/>
         </div>
         );
     }
