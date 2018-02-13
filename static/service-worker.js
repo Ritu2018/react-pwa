396438448256e593b5c3c1697cc19c40cfdcde59
hadeeb <hadeebfarhan1@gmail.com>
2018-02-13 20:28:36 +0530

    Updates

---
+++ static/service-worker.js
@@ -0,0 +1,37 @@
+var CACHE = 'cache-and-update';
+
+self.addEventListener('install', function(evt) {
+    console.log('The service worker is being installed.');
+    evt.waitUntil(precache());
+});
+
+self.addEventListener('fetch', function(evt) {
+    console.log('The service worker is serving the asset.');
+    evt.respondWith(fromCache(evt.request));
+    evt.waitUntil(update(evt.request));
+});
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+      return cache.addAll([
+        '../index.html',
+        './css',
+		'./js',
+		'./media',
+        './depicons'
+      ]);
+    });
+}
+function fromCache(request) {
+    return caches.open(CACHE).then(function (cache) {
+      return cache.match(request).then(function (matching) {
+        return matching || Promise.reject('no-match');
+      });
+    });
+}
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
